b810b4ed2fdec603b87a103a790476d8226fcd59
chenurulfathihah <92660877+FathihahHishamudin@users.noreply.github.com>
2023-07-12 15:54:01 +0800

    favicon
    letak favicon

--- resources/views/layouts/navbar-loggedin.blade.php
+++ resources/views/layouts/navbar-loggedin.blade.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" href="https://comp.utm.my/wp-content/uploads/2018/02/cropped-utm-logo-32x32.png" sizes="32x32">
+    <link rel="icon" href="https://comp.utm.my/wp-content/uploads/2018/02/cropped-utm-logo-192x192.png" sizes="192x192">
+    <link rel="apple-touch-icon" href="https://comp.utm.my/wp-content/uploads/2018/02/cropped-utm-logo-180x180.png">
+    <meta name="msapplication-TileImage" content="https://comp.utm.my/wp-content/uploads/2018/02/cropped-utm-logo-270x270.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     
